c6df1365266cf4af1a4b3e7a7db93c54f323e254
felipeschneider88 <felipes88@gmail.com>
2023-04-28 13:23:27 -0300

    promo dia de la madre

--- components/links/MercadoPagoLink.js
+++ components/links/MercadoPagoLink.js
@@ -5,14 +5,13 @@ import React from 'react'
 const MercadoPagoLink = () => {
   return (
     <Link
-      href="https://mpago.la/2Lsg8kS"
+      href="https://mpago.la/31wJcX8"
       target="_blank"
       rel="noopener noreferrer"
-      className='inline-flex items-center gap-3 py-1 px-3 rounded bg-[#00B1EA]'
-    >
+      className='inline-flex items-center gap-3 py-1 px-3 rounded bg-[#00B1EA]'   >
       <Image src="/svg/mercado-pago.svg" height={20} width={30} alt="" />
-      <span className='font-semibold text-white'>
-        Obtén tu copia
+      <span className='font-semibold text-white'> 
+        Obtén tu promo para el día de la madre
       </span>
     </Link>
   )
